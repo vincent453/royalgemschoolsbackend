34d39ecf164274dcc0bbe6dd15cea5915331a032
Lawrence Vincent <lawrencevincent453@gmail.com>
2026-05-26 09:44:39 +0100

    rename

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,5 +1,5 @@
 import { v2 as cloudinary } from "cloudinary";
-import Blog from "../models/blogModel.js";
+import Blog from "../models/blogModel";
 
 // ── Cloudinary helper (same pattern as studentController) ──────
 const uploadToCloudinary = (buffer) => {
