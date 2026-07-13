958ed7fb9f70d8f96f8ff8fb4e0d3198155726a2
Lawrence Vincent <lawrencevincent453@gmail.com>
2026-07-13 16:12:52 +0100

    -U

--- models/userModel.js
+++ models/userModel.js
@@ -75,10 +75,9 @@ const userSchema = new mongoose.Schema(
 );
 
 // Hash password before saving
-userSchema.pre("save", async function (next) {
-
+userSchema.pre("save", async function () {
   if (!this.isModified("password")) {
-    return next();
+    return;
   }
 
   this.password = await bcrypt.hash(this.password, 10);
